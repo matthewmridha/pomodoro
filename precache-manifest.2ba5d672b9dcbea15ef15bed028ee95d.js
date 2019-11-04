14aa5c74f02eb6ed9b7a70af85942386b6a4439d
matthewmridha <45836769+matthewmridha@users.noreply.github.com>
2019-11-04 07:59:17 +0600

    Updates

---
+++ precache-manifest.2ba5d672b9dcbea15ef15bed028ee95d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cad765d8a6284b4dc0c1fdb8f0b1c3d",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "41b991f19e1a3757f75a",
+    "url": "/pomodoro/static/css/2.b12626c2.chunk.css"
+  },
+  {
+    "revision": "e1810b58c62ce271c015",
+    "url": "/pomodoro/static/css/main.8d133535.chunk.css"
+  },
+  {
+    "revision": "41b991f19e1a3757f75a",
+    "url": "/pomodoro/static/js/2.81fd24d9.chunk.js"
+  },
+  {
+    "revision": "e1810b58c62ce271c015",
+    "url": "/pomodoro/static/js/main.2e65484f.chunk.js"
+  },
+  {
+    "revision": "2b06fd74cafc1de73872",
+    "url": "/pomodoro/static/js/runtime-main.4bd1b737.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pomodoro/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pomodoro/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pomodoro/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pomodoro/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pomodoro/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
